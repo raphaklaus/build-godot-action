f1e8c77526e454d87d28f84750b78ba557da1508
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-03-15 23:00:30 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ unzip Godot_v3.2-stable_export_templates.tpz
 mv templates/* ~/.local/share/godot/templates/3.2.stable
 rm -f Godot_v3.2-stable_export_templates.tpz
 
-godot --export-debug linux ~/build
+godot --export-debug linux ~/build/game
 
 # if [ "$3" != "" ]
 # then
